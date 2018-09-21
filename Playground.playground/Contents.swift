2e104281303d4707ece30050e3fa9a3c4dd998db
Andreas Meingast <ameingast@gmail.com>
2018-09-21 21:41:31 +0200

    Updated Playground to Xcode 10

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -5,7 +5,7 @@ let imageHashing = OSImageHashing.sharedInstance()
 
 // Image Comparison
 
-if let lhs = NSImage(named: NSImage.Name(rawValue: "blurred_architecture1.bmp")), let rhs = NSImage(named: NSImage.Name(rawValue: "compressed_architecture1.jpg")) {
+if let lhs = NSImage(named: NSImage.Name("blurred_architecture1.bmp")), let rhs = NSImage(named: NSImage.Name("compressed_architecture1.jpg")) {
     let lhsHash = imageHashing.hashImage(lhs, with: .dHash)
     let rhsHash = imageHashing.hashImage(rhs, with: .dHash)
     let distance = imageHashing.hashDistance(lhsHash, to: rhsHash, with: .dHash)
