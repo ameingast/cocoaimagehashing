c28eb5f73087cf7468df41ddd1dc8d1e8e2084bf
Andreas Meingast <ameingast@gmail.com>
2019-03-29 11:00:29 -0400

    Xcode 10.2 support

--- CocoaImageHashing/OSFastGraphics.h
+++ CocoaImageHashing/OSFastGraphics.h
@@ -18,19 +18,19 @@ void rotate_rgba_matrix_9_9(unsigned char *pixels);
 
 #pragma mark - Matrix Greyscale Mapping
 
-void greyscale_pixels_rgba_32_32(const unsigned char *pixels, double result[32][32]);
+void greyscale_pixels_rgba_32_32(const unsigned char *pixels, double result[_Nonnull 32][32]);
 
 #pragma mark - DCT
 
-void fast_dct_rgba_32_32(const double pixels[32][32], double result[32][32]);
-void dct_rgba_32_32(const double pixels[32][32], double result[32][32]);
+void fast_dct_rgba_32_32(const double pixels[_Nonnull 32][32], double result[_Nonnull 32][32]);
+void dct_rgba_32_32(const double pixels[_Nonnull 32][32], double result[_Nonnull 32][32]);
 
 double avg_greyscale_value_rgba_8_8(const unsigned char *pixels);
-double fast_avg_no_first_el_rgba_8_8(const double pixels[32][32]);
+double fast_avg_no_first_el_rgba_8_8(const double pixels[_Nonnull 32][32]);
 
 #pragma mark - Perceptual Hashes
 
-OSHashType phash_rgba_8_8(const double pixels[32][32], double dctAverage);
+OSHashType phash_rgba_8_8(const double pixels[_Nonnull 32][32], double dctAverage);
 OSHashType ahash_rgba_8_8(const unsigned char *pixels);
 OSHashType dhash_rgba_9_9(const unsigned char *pixels);
 
