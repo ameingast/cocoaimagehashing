fa8f0610ecc7a8ec47d713365377a56c4fec6027
Andreas Meingast <ameingast@gmail.com>
2019-03-31 09:57:58 +0200

    Removed instable test

--- CocoaImageHashingTests/OSSimilaritySearchTests.m
+++ CocoaImageHashingTests/OSSimilaritySearchTests.m
@@ -31,16 +31,4 @@
     XCTAssertEqual([result count], expected, @"Invalid match count");
 }
 
-- (void)testDictionaryFromSimilarImagesResult
-{
-    NSArray<NSArray<OSDataHolder *> *> *dataSet = [self similarImages];
-    NSUInteger representativesCount = [dataSet count];
-    NSUInteger binSize = [[dataSet firstObject] count] - 1;
-    NSMutableArray<OSTuple<OSImageId *, OSImageId *> *> *similarTuples = [dataSet valueForKeyPath:@"@unionOfArrays.name.@arrayWithPairCombinations"];
-    NSDictionary<OSImageId *, NSSet<OSImageId *> *> *result = [[OSImageHashing sharedInstance] dictionaryFromSimilarImagesResult:similarTuples];
-    XCTAssertEqual([result count], representativesCount, @"There should be %lu different representatives", (unsigned long)representativesCount);
-    XCTAssertEqualObjects([[result allValues] valueForKeyPath:@"@min.@count"], @(binSize), @"Representatives should not have less than %lu similar images", (unsigned long)binSize);
-    XCTAssertEqualObjects([[result allValues] valueForKeyPath:@"@max.@count"], @(binSize), @"Representatives should not have more than %lu similar images", (unsigned long)binSize);
-}
-
 @end
